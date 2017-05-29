b845ad0bbd76616254f50b1af0b4e2c62946b3b8
Sunny Karira <sunnykarira@outlook.com>
2017-05-29 11:59:47 +0530

    Variables and data types

---
+++ Basics/script.js
@@ -0,0 +1,23 @@
+var name = 'John';
+console.log(name);
+
+//CamelCasing
+var lastName = 'Smith';
+console.log(lastName);
+
+var age = 26;
+console.log(age);
+
+//Data Types
+/*
+Number
+String
+Boolean
+Undefined
+Null
+*/
+
+var fullAge = true;
+console.log(fullAge);
+
+
